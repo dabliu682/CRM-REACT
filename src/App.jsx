7d318b2f4a6184267aa37f0e8d8e390556c8dea0
dabliu682 <dabliu.682@gmail.com>
2022-04-26 23:20:54 -0500

    Creacion de rutas layout y paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './layout/Layout'
+import Inicio from './paginas/Inicio'
+import EditarCliente from './paginas/EditarCliente'
+import NuevoCliente from './paginas/NuevoCliente'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <h1>crm react</h1>
+    <BrowserRouter>
+      <Routes>        
+        <Route path='/clientes' element={<Layout/>}>
+          <Route index element={<Inicio/>}/>
+          <Route path='nuevo' element={<NuevoCliente/>}/>
+          <Route path='editar:id' element={<EditarCliente/>}/>          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
